bc38d2bb929e4d2bd192f32a2f313729b991741a
FauziHidayat <hidayat.fauzi52@gmail.com>
2022-01-26 18:19:33 +0700

    JobIntentService

--- app/src/main/java/com/projectku/myservice/MainActivity.kt
+++ app/src/main/java/com/projectku/myservice/MainActivity.kt
@@ -13,13 +13,15 @@ class MainActivity : AppCompatActivity() {
 
         val btnStartService = findViewById<Button>(R.id.btn_start_service)
         btnStartService.setOnClickListener {
-            val mStartServiceIntent  = Intent(this, MyService::class.java)
+            val mStartServiceIntent  = Intent(this@MainActivity, MyService::class.java)
             startService(mStartServiceIntent)
         }
 
         val btnStartJobIntentService = findViewById<Button>(R.id.btn_start_job_intent_service)
         btnStartJobIntentService.setOnClickListener {
-
+            val mStartIntentService = Intent(this, MyJobIntentService::class.java)
+            mStartIntentService.putExtra(MyJobIntentService.EXTRA_DURATION, 5000L)
+            MyJobIntentService.enqueueWork(this, mStartIntentService)
         }
 
         val btnStartBoundService = findViewById<Button>(R.id.btn_start_bound_service)
